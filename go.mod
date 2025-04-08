6ffa595fc17c027ea7677655137a1ac2ccfadbe9
Siddhant Madhur <siddhant.madhur@gmail.com>
2025-04-08 13:05:19 -0700

    added seperate docs

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/siddhantmadhur/ocelot-media-server
 go 1.24.0
 
 require (
+	golang.org/x/crypto v0.36.0
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	golang.org/x/text v0.23.0 // indirect
-	gorm.io/driver/sqlite v1.5.7 // indirect
-	gorm.io/gorm v1.25.12 // indirect
 )
--- internal/auth/user.go
+++ internal/auth/user.go
@@ -1,19 +1,42 @@
 package auth
 
 import (
+	"errors"
 	"log"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	Id                uint   `json:"id" gorm:"primaryKey"`
 	Username          string `json:"username" gorm:"unique;not null"`
+	DisplayName       string `json:"display_name" gorm:"not null"`
 	EncryptedPassword string `json:"-" gorm:"not null"`
+	Permission        int    `json:"permission" gorm:"not null"`
 }
 
 func UpdateModels(tx *gorm.DB) error {
 	log.Printf("[AUTH] Auto migrating models to database...\n")
 	err := tx.AutoMigrate(&User{})
 	return err
+}
+
+func CreateUser(username string, displayName string, password string) (User, error) {
+	var u User
+
+	if len(username) <= 3 || len(username) >= 14 {
+		return u, errors.New("Username is not within 3 and 14 characters")
+	}
+
+	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return u, err
+	}
+
+	u.EncryptedPassword = string(encryptedPassword)
+	u.DisplayName = displayName
+	u.Username = username
+
+	return u, err
 }
